7f2e8ca495112aff32c2f864a81a57b9d6896a6f
Posijour <168458508+Posijour@users.noreply.github.com>
2026-01-09 18:23:20 +0200

    Update config.py

--- config.py
+++ config.py
@@ -3,9 +3,6 @@ import os
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 COINGLASS_API_KEY = os.getenv("COINGLASS_API_KEY")
 
-SYMBOLS = [
-    "BTCUSDT",
-    "ETHUSDT",
-]
+SYMBOLS = ["BTCUSDT", "ETHUSDT"]
 INTERVAL_SECONDS = 300  # 5 минут
 RISK_ALERT_LEVEL = 6
